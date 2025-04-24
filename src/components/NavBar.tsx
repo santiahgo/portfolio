e83c596806729826e703c63a2eb3817595bc5a8a
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-04-24 14:56:07 -0400

    good enough for now

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,14 @@
 export function NavBar() {
 	return (
-		<nav className="flex justify-end p-2">
-			<ul className="flex gap-4">
-				<a href="#" className="text-button-link normal-text p-3">
+		<nav className="flex justify-end py-3">
+			<ul className="flex gap-4 me-5">
+				<a href="#" className="text-button-link normal-text px-3 py-4">
 					<li>Experience</li>
 				</a>
-				<a href="#" className="text-button-link normal-text p-3">
+				<a href="#" className="text-button-link normal-text px-3 py-4">
 					<li>Projects</li>
 				</a>
-				<a href="#" className="text-button-link normal-text p-3">
+				<a href="#" className="text-button-link normal-text px-3 py-4">
 					<li>Contact</li>
 				</a>
 			</ul>

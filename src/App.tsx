6a393fb911c46aeb2d042b8435dd44a4092cd65b
santiahgo <sorozcob@charlotte.edu>
2025-04-26 14:15:24 -0400

    updating app to include experience component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from './providers/ThemeProvider';
 import { NavBar } from './components/NavBar';
 import { Introduction } from './components/Introduction';
+import { Experience } from './components/Experience';
 
 function App() {
 	return (
@@ -8,14 +9,12 @@ function App() {
 			<div className="bg-background min-h-screen w-screen">
 				<div className="m-auto w-7xl">
 					<NavBar />
-					<div className="flex gap-10">
-						<div className='w-full mt-9'>
+					<div className="mt-9 flex gap-10">
+						<div className="w-full">
 							<Introduction />
 						</div>
-						<div className='flex justify-center w-full'>
-							{/* 
-								gonna have experience, projects, and contact components
-							*/}
+						<div className="flex w-full">
+							<Experience />
 						</div>
 					</div>
 				</div>

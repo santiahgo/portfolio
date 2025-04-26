3f1d5e574ae076c91f2bbf1d5ed25d76708a622b
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-04-25 20:50:44 -0400

    updated navbar

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,7 +5,7 @@ interface IconProps {
 
 export const Icon = ({ icon, language }: IconProps) => {
 	return (
-		<div className="text-button-link flex flex-col gap-0.5 items-center">
+		<div className="text-button-link caption flex flex-col gap-1 items-center w-[50px]">
 			{icon}
 			{language}
 		</div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,21 @@
+import logo from '../assets/images/Santiago.svg'
+
 export const NavBar = () => {
 	return (
-		<nav className="flex justify-end pt-5">
-			<ul className="flex gap-4">
-				<a href="#" className="text-button-link normal-text px-3 py-4">
+		<nav className="flex justify-between items-center pt-8">
+			<div>
+				<a href="#">
+					<img src={logo} alt="logo" />
+				</a>
+			</div>
+			<ul className="flex gap-9">
+				<a href="#" className="text-button-link normal-text">
 					<li>Experience</li>
 				</a>
-				<a href="#" className="text-button-link normal-text px-3 py-4">
+				<a href="#" className="text-button-link normal-text">
 					<li>Projects</li>
 				</a>
-				<a href="#" className="text-button-link normal-text px-3 py-4">
+				<a href="#" className="text-button-link normal-text">
 					<li>Contact</li>
 				</a>
 			</ul>

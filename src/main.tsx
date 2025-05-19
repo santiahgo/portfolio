daa7b4feb474bce6c174454bda12837e3ff92a3b
santiahgo <sorozcob@charlotte.edu>
2025-05-19 15:04:15 -0400

    ignore, i just moved a line of code

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { ToastContainer } from 'react-toastify';
 import './index.css';
+import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from './providers/ThemeProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(

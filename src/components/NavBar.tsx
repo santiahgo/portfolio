e2f8ec3928ad63d889e094bbfce993bf0317bfb2
santiahgo <sorozcob@charlotte.edu>
2025-05-19 16:05:35 -0400

    added toggler to toggle between dark and light modes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import logo from '../assets/images/Santiago.svg';
+import { ThemeModeToggle } from './ThemeModeToggle';
 
 interface NavBarProps {
 	active: string;
@@ -34,22 +35,23 @@ export const NavBar = ({ active, handleSetActiveLink }: NavBarProps) => {
 					<img src={logo} alt="logo" />
 				</a>
 			</div>
-			<ul className="flex gap-9">
-				<a
-					href="#"
-					className={`text-button-link normal-text hover:text-primary-text border-b-2 border-transparent transition-all duration-300 ease-in-out ${active === 'experience' ? 'border-b-primary-text text-primary-text border-b-2' : ''}`}
-					onClick={() => handleSetActiveLink('experience')}
-				>
-					<li>Experience</li>
-				</a>
-				<a
-					href="#"
-					className={`text-button-link normal-text hover:text-primary-text border-b-2 border-transparent transition-all duration-300 ease-in-out ${active === 'projects' ? 'border-b-primary-text text-primary-text border-b-2' : ''}`}
-					onClick={() => handleSetActiveLink('projects')}
-				>
-					<li>Projects</li>
-				</a>
-			</ul>
+			<div className="flex items-center gap-9">
+				<ul className="flex gap-9">
+					<a
+						className={`text-button-link normal-text hover:text-primary-text hover:cursor-pointer border-b-2 border-transparent transition-all duration-300 ease-in-out ${active === 'experience' ? 'border-b-primary-text text-primary-text border-b-2' : ''}`}
+						onClick={() => handleSetActiveLink('experience')}
+					>
+						<li>Experience</li>
+					</a>
+					<a
+						className={`text-button-link normal-text hover:text-primary-text hover:cursor-pointer border-b-2 border-transparent transition-all duration-300 ease-in-out ${active === 'projects' ? 'border-b-primary-text text-primary-text border-b-2' : ''}`}
+						onClick={() => handleSetActiveLink('projects')}
+					>
+						<li>Projects</li>
+					</a>
+				</ul>
+				<ThemeModeToggle />
+			</div>
 		</nav>
 	);
 };

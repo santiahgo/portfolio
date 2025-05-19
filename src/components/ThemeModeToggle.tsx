1392b2d02171d76fa1fffaf4728fb7583dac3a0a
santiahgo <sorozcob@charlotte.edu>
2025-05-19 16:07:57 -0400

    custom toggler to toggle website theme modes

--- src/components/ThemeModeToggle.tsx
+++ src/components/ThemeModeToggle.tsx
@@ -1,2 +1,37 @@
-// will toggle between light and dark mode
-// https://youtu.be/r_zqgg4FsgE?si=LWrfrZREGDKSsYvu
+import { useTheme } from '../providers/ThemeProvider';
+import { GoMoon } from 'react-icons/go';
+import { GoSun } from 'react-icons/go';
+
+export const ThemeModeToggle = () => {
+	const { theme, setTheme } = useTheme();
+
+	const isDark = theme.mode === 'dark';
+
+	const toggleTheme = () => {
+		setTheme({ mode: isDark ? 'light' : 'dark' });
+	};
+
+	return (
+		<label
+			htmlFor="theme-toggle"
+			className="bg-button-link relative inline-flex h-9 w-20 cursor-pointer items-center justify-between rounded-full px-2"
+		>
+			<GoSun className="z-10 text-yellow-400 translate-x-0.5" />
+			<GoMoon className="z-10 text-indigo-400 -translate-x-1.5" />
+
+			<input
+				type="checkbox"
+				id="theme-toggle"
+				className="sr-only"
+				checked={isDark}
+				onChange={toggleTheme}
+			/>
+
+			<span
+				className={`absolute top-1 left-1 h-7 w-7 rounded-full bg-white shadow-md transition-transform duration-300 ${
+					isDark ? 'translate-x-10' : 'translate-x-0'
+				}`}
+			></span>
+		</label>
+	);
+};

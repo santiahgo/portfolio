cdf8af31a8a50ea9e29f88f977825e3cb884afc1
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-05-27 20:01:43 -0400

    using localStorage to save preference and moved the theme className and attribute to the html tag

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useLayoutEffect, useState } from 'react';
 
 const initialState: ThemeProviderState = {
 	theme: { mode: 'dark' },
@@ -9,39 +9,40 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
 	children,
-	defaultTheme = { mode: 'dark' },
+	defaultTheme = { mode: 'system' },
 	...props
 }: ThemeProviderProps) {
-	const [storedTheme, setStoredTheme] = useState<Theme>(defaultTheme);
+	const [storedTheme, setStoredTheme] = useState<Theme>(() => {
+		const savedTheme = localStorage.getItem('theme');
+		return savedTheme ? { mode: savedTheme as ThemeMode } : defaultTheme;
+	});
 
 	const value = {
 		theme: storedTheme,
 		setTheme: (theme: Theme) => {
+			localStorage.setItem('theme', theme.mode);
 			setStoredTheme(theme);
 		},
 	};
 
-	if (storedTheme.mode === 'system') {
-		const systemMode = window.matchMedia('(prefers-color-schema: dark)').matches
-			? 'dark'
-			: 'light';
-		return (
-			<ThemeProviderContext.Provider { ...props } value={ value }>
-				<div className={ `${ storedTheme.mode }` } data-theme={ `${ systemMode }` }>{ children }</div>
-			</ThemeProviderContext.Provider>
-		);
-	}
+	useLayoutEffect(() => {
+		const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+		const mode =
+			storedTheme.mode === 'system' ? (prefersDark ? 'dark' : 'light') : storedTheme.mode;
 
-    return (
-        <ThemeProviderContext.Provider { ...props } value={ value }>
-            <div className={ `${ storedTheme.mode }` } data-theme={ `${ storedTheme.mode }` }>{ children }</div>
-        </ThemeProviderContext.Provider>
-    );
+		document.documentElement.className = storedTheme.mode;
+		document.documentElement.setAttribute('data-theme', mode);
+	}, [storedTheme]);
+
+	return (
+		<ThemeProviderContext.Provider {...props} value={value}>
+			{children}
+		</ThemeProviderContext.Provider>
+	);
 }
 
 export const useTheme = () => {
-    const context = useContext(ThemeProviderContext);
-    if (context === undefined) throw new Error('useTheme must be within a ThemeProvider');
-
-    return context;
-}
+	const context = useContext(ThemeProviderContext);
+	if (context === undefined) throw new Error('useTheme must be within a ThemeProvider');
+	return context;
+};

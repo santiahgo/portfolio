c015750ee717b05d8b3b20a9c9f3ed7248071370
santiahgo <sorozcob@charlotte.edu>
2025-06-02 16:39:32 -0400

    decreased the top margin

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
 		<div className="bg-background min-h-screen w-full">
 			<div className="mx-auto max-w-screen-xl px-4 sm:px-6 md:px-8">
 				<NavBar active={activeLink} handleSetActiveLink={handleSetActiveLink} />
-				<div className="mt-8 flex flex-col gap-10 xl:grid xl:grid-cols-2">
+				<div className="mt-5 flex flex-col gap-10 xl:grid xl:grid-cols-2">
 					<aside className="w-full self-start xl:sticky xl:top-[121px]">
 						<Introduction />
 					</aside>

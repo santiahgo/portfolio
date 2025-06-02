d0a112ca50edc9f4e278a778cbd9d6e226209fd0
santiahgo <sorozcob@charlotte.edu>
2025-06-02 11:27:08 -0400

    created a hook to check if the user is on mobile

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+export const useIsMobile = (breakpoint = 640) => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < breakpoint);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [breakpoint]);
+
+    return isMobile;
+}

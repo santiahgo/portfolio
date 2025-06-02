7c181639e3c0d66209a3f812169792f20c3039e8
santiahgo <sorozcob@charlotte.edu>
2025-06-02 16:47:33 -0400

    converted mobile nav to a hamburger navbar and made the nav links smooth scroll the correct section

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
+import { IoCloseOutline } from 'react-icons/io5';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import logo from '../assets/images/Santiago.svg';
 import { ThemeModeToggle } from './ThemeModeToggle';
-import { RxHamburgerMenu } from 'react-icons/rx';
-import { IoCloseOutline } from 'react-icons/io5';
 
 interface NavBarProps {
 	active: string;
@@ -31,14 +31,13 @@ export const NavBar = ({ active, handleSetActiveLink }: NavBarProps) => {
 
 	return (
 		<nav
-			className={`sticky z-10 duration-75 ${isScrolled ? 'bg-background' : 'bg-transparent'} top-0 flex items-center justify-between py-6 transition-colors`}
-			id="navbar"
+			className={`sticky z-10 duration-75 ${isScrolled ? 'bg-background' : 'bg-transparent'} top-0 flex w-full items-center justify-between py-6 transition-colors`}
 		>
 			<div>
 				<img src={logo} alt="logo" />
 			</div>
 
-			<div className="hidden sm:flex items-center gap-9">
+			<div className="hidden items-center gap-9 sm:flex">
 				<ul className="flex gap-9">
 					<a
 						className={`text-button-link normal-text hover:text-primary-text border-b-2 border-transparent transition-all duration-300 ease-in-out hover:cursor-pointer ${active === 'experience' ? 'border-b-primary-text text-primary-text border-b-2' : ''}`}
@@ -56,39 +55,51 @@ export const NavBar = ({ active, handleSetActiveLink }: NavBarProps) => {
 				<ThemeModeToggle />
 			</div>
 
-
 			<div className="flex items-center gap-4 sm:hidden">
 				<ThemeModeToggle />
-				<button onClick={() => setShowMobileMenu(!showMobileMenu)} className="text-button-link">
-					{showMobileMenu ? <IoCloseOutline size={24} /> : <RxHamburgerMenu size={24} />}
+				<button
+					onClick={() => setShowMobileMenu(!showMobileMenu)}
+					className="text-button-link hover:cursor-pointer"
+				>
+					{!showMobileMenu && <RxHamburgerMenu size={24} />}
 				</button>
 			</div>
 
 			{/* mobile menu */}
-			{showMobileMenu && (
-				<div className="absolute top-full left-0 w-full bg-background shadow-md sm:hidden">
-					<ul className="flex flex-col items-center gap-4 py-4">
-						<a
-							className={`text-button-link normal-text hover:text-primary-text border-b-2 border-transparent transition-all duration-300 ease-in-out hover:cursor-pointer ${active === 'experience' ? 'border-b-primary-text text-primary-text border-b-2' : ''}`}
-							onClick={() => {
-								handleSetActiveLink('experience')
-								setShowMobileMenu(false);
-							}}
-						>
-							<li>Experience</li>
-						</a>
-						<a
-							className={`text-button-link normal-text hover:text-primary-text border-b-2 border-transparent transition-all duration-300 ease-in-out hover:cursor-pointer ${active === 'projects' ? 'border-b-primary-text text-primary-text border-b-2' : ''}`}
-							onClick={() => {
-								handleSetActiveLink('projects')
-								setShowMobileMenu(false);
-							}}
-						>
-							<li>Projects</li>
-						</a>
-					</ul>
-				</div>
-			)}
+			<div
+				className={`bg-background fixed inset-0 z-50 flex flex-col items-center justify-center transition-transform duration-300 sm:hidden ${
+					showMobileMenu ? 'translate-y-0' : '-translate-y-full'
+				}`}
+			>
+				<button
+					className="text-button-link absolute top-8 right-5 hover:cursor-pointer"
+					onClick={() => setShowMobileMenu(false)}
+				>
+					<IoCloseOutline size={24} />
+				</button>
+				<ul className="flex h-full flex-col items-center justify-center gap-4">
+					<a
+						href="#experience"
+						className={`text-button-link normal-text hover:text-primary-text border-b-2 border-transparent transition-all duration-300 ease-in-out hover:cursor-pointer`}
+						onClick={() => {
+							handleSetActiveLink('experience');
+							setShowMobileMenu(false);
+						}}
+					>
+						<li>Experience</li>
+					</a>
+					<a
+						href="#projects"
+						className={`text-button-link normal-text hover:text-primary-text border-b-2 border-transparent transition-all duration-300 ease-in-out hover:cursor-pointer`}
+						onClick={() => {
+							handleSetActiveLink('projects');
+							setShowMobileMenu(false);
+						}}
+					>
+						<li>Projects</li>
+					</a>
+				</ul>
+			</div>
 		</nav>
 	);
 };

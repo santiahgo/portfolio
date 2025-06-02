976adf0be14ac86efb580a090bacd0c5288fccba
santiahgo <sorozcob@charlotte.edu>
2025-06-02 11:28:18 -0400

    shows everything on one screen so that navbar basically becomes obsolete on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { useIsMobile } from './hooks/useIsMobile';
 import { Experience } from './components/Experience';
 import { Introduction } from './components/Introduction';
 import { NavBar } from './components/NavBar';
@@ -8,6 +9,7 @@ import { Projects } from './components/Projects';
 function App() {
 	const [activeLink, setActiveLink] = useState('experience');
 	const isFirstRender = useRef(true);
+	const isMobile = useIsMobile();
 
 	const handleSetActiveLink = useCallback(
 		(link: string) => {
@@ -44,44 +46,52 @@ function App() {
 						<Introduction />
 					</aside>
 					<section className="w-full">
-						<AnimatePresence mode="wait">
-							{activeLink === 'experience' && (
-								<motion.div
-									key="experience"
-									variants={styles}
-									initial="initial"
-									animate="animate"
-									exit="exit"
-									transition={{
-										duration: 0.5,
-										type: 'spring',
-										bounce: 0.2,
-										delay: isFirstRender.current ? 0.5 : 0,
-									}}
-									className="w-full"
-								>
+						{
+							isMobile ? (
+								<div className='w-full grid gap-7'>
 									<Experience />
-								</motion.div>
-							)}
-							{activeLink === 'projects' && (
-								<motion.div
-									key="projects"
-									variants={styles}
-									initial="initial"
-									animate="animate"
-									exit="exit"
-									transition={{
-										duration: 0.5,
-										type: 'spring',
-										bounce: 0.2,
-										delay: isFirstRender.current ? 0.5 : 0,
-									}}
-									className="w-full"
-								>
 									<Projects />
-								</motion.div>
-							)}
-						</AnimatePresence>
+								</div>
+							): 
+							<AnimatePresence mode="wait">
+								{activeLink === 'experience' && (
+									<motion.div
+										key="experience"
+										variants={styles}
+										initial="initial"
+										animate="animate"
+										exit="exit"
+										transition={{
+											duration: 0.5,
+											type: 'spring',
+											bounce: 0.2,
+											delay: isFirstRender.current ? 0.5 : 0,
+										}}
+										className="w-full"
+									>
+										<Experience />
+									</motion.div>
+								)}
+								{activeLink === 'projects' && (
+									<motion.div
+										key="projects"
+										variants={styles}
+										initial="initial"
+										animate="animate"
+										exit="exit"
+										transition={{
+											duration: 0.5,
+											type: 'spring',
+											bounce: 0.2,
+											delay: isFirstRender.current ? 0.5 : 0,
+										}}
+										className="w-full"
+									>
+										<Projects />
+									</motion.div>
+								)}
+							</AnimatePresence>
+						}
 					</section>
 				</div>
 			</div>
